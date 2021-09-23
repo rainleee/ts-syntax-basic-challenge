c0ce606a4c9a67cd2ae5f4d029faf159a711b8cd
rainleee <rainlee4401@gmail.com>
2021-09-23 18:23:05 +0900

    feat: add game.ts func move()

--- game.ts
+++ game.ts
@@ -1,6 +1,37 @@
 /**
  * Let's make a game 🕹
  */
+
+type Direction = 'up' | 'down' | 'left' | 'right';
+
+type Position = {
+    x: number;
+    y: number;
+}
+
+const position: Position = {
+    x: 0,
+    y: 0
+}
+
+function move(direction: Direction){
+    switch(direction){
+        case 'up':
+             position.y += 1;
+             break;
+        case 'down':
+             position.y -= 1;
+             break;
+        case 'left':
+             position.x -= 1;
+             break;
+        case 'right':
+             position.x += 1;
+             break;
+        default :
+        throw new Error(`unknown direction: ${direction}`);
+    }
+}
 console.log(position); // { x: 0, y: 0}
 move('up');
 console.log(position); // { x: 0, y: 1}
