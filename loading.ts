57a84a2c0ec0b51c96d34e1ec42c69bb6ce55c6b
rainleee <rainlee4401@gmail.com>
2021-09-23 18:35:48 +0900

    feat: add loading.ts printLoginState()

--- loading.ts
+++ loading.ts
@@ -20,6 +20,34 @@
 
   type ResourceLoadState = LoadingState | SuccessState | FailState;
 
+  function printLoginState(state: ResourceLoadState){
+
+    // if(state.state === 'loading'){
+    //   return console.log(`👀 ${state.state}`);
+    // }else if(state.state === 'success'){
+    //   return console.log(`😃 ${state.response.body}`);
+    // }else{
+    //   return console.log(`😱 ${state.reason}`);
+    // }
+
+    switch(state.state){
+      case 'loading' : 
+        console.log(`👀 ${state.state}`);
+        break;
+      case 'success' : 
+        console.log(`😃 ${state.response.body}`);
+        break;
+      case 'fail' : 
+        console.log(`😱 ${state.reason}`);
+        break;
+      default:
+        throw new Error(`unknown state : ${state}`);
+      
+    }
+
+
+  }
+
   printLoginState({ state: 'loading' }); // 👀 loading...
   printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
   printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
